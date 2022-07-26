c549ad578e6d0d779929e3236f5f9d4c57ff5d24
Ricardo Said <plrs.said.perez@outlook.com>
2022-07-26 15:31:41 -0500

    se agregaron sonidos para darle efectos

--- main.js
+++ main.js
@@ -12,8 +12,12 @@ let mostrarMovimientos = document.querySelector('#movimientos');
 let mostarAciertos = document.querySelector('#aciertos');
 let btnReiniciar = document.querySelector('.btn-reiniciar');
 let btns= document.querySelectorAll('.btn-cuadros');
-console.log(btns);
 
+// objetos de audio
+let aciertoAudio = new Audio('/estilos/sounds/acierto.wav');
+let clicktoAudio = new Audio('/estilos/sounds/click.wav');
+let errortoAudio = new Audio('/estilos/sounds/error.wav');
+let ganoAudio = new Audio('/estilos/sounds/gano.wav');
 
 
 
@@ -30,32 +34,38 @@ function reiniciarJuego() {
     aciertos=0;
     movimientos=0;
     numeros = numeros.sort(()=>{return Math.random()-.5});
+    console.log(numeros);
     return numeros;
 }
 // generar el arreglo de numeros aleatorios
 numeros = numeros.sort(()=>{return Math.random()-.5});
-
+console.log(numeros);
 
 // funcion principal
 function destapar(id) {
     tarjetasDestapadas++;
 
+
     
     if (tarjetasDestapadas === 1) {
         // mostrar el numero que seleccionamos
         tarjeta1 = document.getElementById(id);
+        console.log(tarjeta1)
         primerResultado= numeros[id];
-        tarjeta1.innerHTML = primerResultado;
+        tarjeta1.innerHTML = `<img src="/estilos/img/${primerResultado}.png" alt="">`;
 
         // desabilitar el boton seleccionado
         // para no aumentar el cotador con el mismo
         tarjeta1.disabled = true;
+        clicktoAudio.play();
     }
     else if (tarjetasDestapadas === 2) {
         tarjet2 = document.getElementById(id);
+        console.log(tarjet2);
         segundoResultado = numeros[id];
-        tarjet2.innerHTML = segundoResultado;
+        tarjet2.innerHTML = `<img src="/estilos/img/${segundoResultado}.png" alt="">`;
         tarjet2.disabled= true;
+        clicktoAudio.play();
 
     // incrementar moviminetos 
     movimientos++;
@@ -67,8 +77,15 @@ function destapar(id) {
             // aumentar aciertos
             aciertos++;
             mostarAciertos.innerHTML= `Aciertos: ${aciertos}`;
+            setTimeout(() => {
+                 aciertoAudio.play();
+            }, 300);
             if (aciertos == 8) {
-                mostarAciertos.textContent= `Acabas de Ganar!!;`
+                mostarAciertos.textContent= `Acabas de Ganar!!`
+                setTimeout(() => {
+                  ganoAudio.play();  
+                }, 500);
+                
             }
             
 
@@ -80,7 +97,9 @@ function destapar(id) {
                 tarjeta1.disabled= false,
                 tarjet2.disabled = false;
                 tarjetasDestapadas = 0;
+                errortoAudio.play();
             }, 800);
+            
         }
 
     } 
